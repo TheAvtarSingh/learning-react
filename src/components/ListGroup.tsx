11ad1c18b97f331c8c09e2eeb0c724a59ab839ad
TheAvtarSingh <singha2k2@gmail.com>
2023-05-29 00:25:03 +0530

    Added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,46 @@
+import { MouseEvent } from "react";
+
 export default function ListGroups() {
-const projects = ["WEAPPL - Weather Mobile Application with REST API","Student Result Management System - MERN Stack","Catelog Mobile Application - Flutter","Hotel Booking Website - Travelling Project","Toyshop Ecommerce Web Application - MEAN STACK"];
+  let projects = [
+    "WEAPPL - Weather Mobile Application with REST API",
+    "Student Result Management System - MERN Stack",
+    "Catelog Mobile Application - Flutter",
+    "Hotel Booking Website - Travelling Project",
+    "Toyshop Ecommerce Web Application - MEAN STACK",
+  ];
+
+  // Conditional Rendering
+  // projects =[];
+
+  /* const getMessage = () => {
+    return projects.length === 0 ? <p>No Items Found</p> : null;
+} */
+
+const handleClick = (event:MouseEvent) => {
+    console.log(event);
+  }
 
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-            {projects.map((projects)=>(
-                <li key={projects} className="list-group-item">{projects}</li>
-            ))}
-          {/* <li className="list-group-item">Cras justo odio</li>
+      <h1>List</h1>
+      {/* {getMessage()} */}
+      {projects.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {projects.map((projects, index) => (
+          <li
+            key={projects}
+            onClick={handleClick}
+            className="list-group-item"
+          >
+            {projects}
+          </li>
+        ))}
+        {/* <li className="list-group-item">Cras justo odio</li>
           <li className="list-group-item">Dapibus ac facilisis in</li>
           <li className="list-group-item">Morbi leo risus</li>
           <li className="list-group-item">Porta ac consectetur ac</li>
           <li className="list-group-item">Vestibulum at eros</li> */}
-        </ul>
+      </ul>
     </>
   );
 }
